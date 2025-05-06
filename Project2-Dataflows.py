1da3ec2ef6ef1134a9eb436cd431644ff7d109c6
eeshajain1 <57434299+eeshajain1@users.noreply.github.com>
2025-05-06 02:06:13 -0700

    Create Project2-Dataflows.py

---
+++ Project2-Dataflows.py
@@ -0,0 +1,85 @@
+import math
+
+
+#CNN Layer Configs, (num_filters (output channels), num_channels (input channels), kernel_h, kernel_w), only consider the convolutional layers for our calcualtions
+layers = [
+    (32, 3, 3, 3),
+    (64, 32, 3, 3),
+    (128, 64, 3, 3),
+    (256, 128, 3, 3),
+    (512, 256, 3, 3)
+]
+
+#output spatial dimensions after each layer (with pooling after layers 2 and 4)
+#remember its 32x32 32x32 16x16 16x16 16x16 for the height and width 
+image_dims = [32, 32, 16, 16, 16]
+
+#system specs
+activation_bitwidth = 8
+weight_bitwidth = 8 
+DRAM_access_energy = 4e-12 #4 pj/bit
+SRAM_access_energy = 0.1e-12 #0.1 pj/bit
+num_DPUs = 16
+DPU_size = 128 
+single_DPU_energy_per_cycle = 20e-12 #20pJ
+clock_period = 1e-9 #1 ns
+
+# All latency number here are number of cycles required to do the operation. And for memory
+# load it means the latency to read the all required data from the SRAM to buffer, same for
+# memory write. Computation latency means the latency required to perform one dot product
+# operation for all 16 dot product engines in parallel. DRAM latency means the latency required to
+# load weights from all layers into weight SRAM.
+
+DRAM_access_latency = 7000
+weight_SRAM_load_latency = 6
+activation_SRAM_load_latency = 3
+activation_SRAM_write_latency = 3
+compute_latency = 2
+
+n_dot_product_units = 16
+dot_product_unit_size = 128
+
+
+def compute_layer_energy(dataflow, batch_size):
+    total_energy = 0
+
+    for i, (n_filter, n_channel, kernel_size, S) in enumerate(layers):
+        H = W = image_dims[i]
+        
+
+        if dataflow == 'IS':  #Input Stationary
+            #inputs are only loaded in once, so we do # of inputs / # of dpus and these are stationary
+            #one input per dpu
+            #kind of WS reversed 
+            NIBU = math.ceil(n_channel * kernel_size/ n_dot_product_units) * math.ceil( n_filter / dot_product_unit_size)
+            NDPC = NIBU * H * W * batch_size
+            ISE = NIBU * n_dot_product_units * dot_product_unit_size * activation_bitwidth * SRAM_access_energy * batch_size
+            WSE = NDPC * dot_product_unit_size * weight_bitwidth * SRAM_access_energy
+            OSWE = NDPC * n_dot_product_units * activation_bitwidth * SRAM_access_energy
+            OSRE = (17/18) * OSWE
+            OSE = OSRE + OSWE
+            DPE = NDPC * n_dot_product_units * single_DPU_energy_per_cycle
+            total_energy += WSE + ISE + OSE + DPE
+
+        elif dataflow == 'OS':  #Output Stationary
+            #we do not need NOBU because partial sums are kept
+            # local to the array and only written back to SRAM 
+            # after accumulation is complete
+            #NDPC is equal to the number of buffer updates (I think)
+            NDPC = n_filter * H * W * batch_size #for each filter a full output is produced
+            ISE = NDPC * dot_product_unit_size * activation_bitwidth * SRAM_access_energy
+            WSE = NDPC * dot_product_unit_size * weight_bitwidth * SRAM_access_energy
+            OSE = NDPC * activation_bitwidth * SRAM_access_energy  #only write it once
+            DPE = NDPC * n_dot_product_units * single_DPU_energy_per_cycle
+            total_energy += ISE + WSE + OSE + DPE
+
+    print(f"dataflow = {dataflow}, batch size = {batch_size}, energy = {total_energy} J")
+
+
+#1a
+compute_layer_energy('IS', 1)
+compute_layer_energy('IS', 256)
+compute_layer_energy('OS', 1)
+compute_layer_energy('OS', 256)
+
+
